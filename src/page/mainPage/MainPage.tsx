679fae2f117e7c00b2e7eba60ade7f2f8dd32b71
minwoo1119 <minwoo1119@naver.com>
2025-08-29 14:08:36 +0900

    [Feat] #1 - 메인 헤더 생성

--- src/page/mainPage/MainPage.tsx
+++ src/page/mainPage/MainPage.tsx
@@ -1,8 +1,10 @@
+import { Header } from '../../components/common/headers/Header';
 import styles from './mainpage.module.scss';
 
 function MainPage() {
   return (
     <div className={styles.container}>
+      <Header />
       <div>hi This is Main Page</div>
     </div>
   );
