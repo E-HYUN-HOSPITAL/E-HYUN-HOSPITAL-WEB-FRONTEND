70a0cfecb5cf309ded1d2963d7e00cd407b3209f
minwoo1119 <minwoo1119@naver.com>
2025-09-03 19:44:40 +0900

    [Feat] #9 - 카카오 상담 버튼 컴포넌트 구현

---
+++ src/components/common/goKakaoBtn/GoKakaoBtn.tsx
@@ -0,0 +1,13 @@
+import styles from './goKakaoBtn.module.scss';
+
+type Props = {
+  onClick: () => void;
+};
+
+export const GoKakaoBtn = ({ onClick }: Props) => {
+  return (
+    <div className={styles.container} onClick={onClick}>
+      <img src='public/kakao_icon.svg' alt='카카오 아이콘' />
+    </div>
+  );
+};
