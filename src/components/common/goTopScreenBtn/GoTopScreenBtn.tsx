0951dc4ff6bac3581a32bab33e4b1a03999abb1f
minwoo1119 <minwoo1119@naver.com>
2025-09-03 19:44:56 +0900

    [Feat] #9 - 최상단 이동 버튼 컴포넌트 구현

---
+++ src/components/common/goTopScreenBtn/GoTopScreenBtn.tsx
@@ -0,0 +1,20 @@
+import styles from './goTopScreenBtn.module.scss';
+
+export const GoTopScreenBtn = () => {
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <img
+        src='public/go_top_btn.svg'
+        alt='위로가기'
+        onClick={handleScrollToTop}
+      />
+    </div>
+  );
+};
