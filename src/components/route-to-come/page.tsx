e756f3b93814c2aafca77bf7e10a5b54afe9d777
seoyeonii <seoyeonii@ajou.ac.kr>
2025-09-11 18:09:33 +0900

    [Feat] #6 - 카카오맵 띄우기
    index.html에 map <script>태그 추가, map.tsx kakao 전역 설정, 일부 태그 수정

--- src/components/route-to-come/page.tsx
+++ src/components/route-to-come/page.tsx
@@ -1,10 +1,30 @@
 import { ToolBar } from "../common/toolBar/ToolBar";
 import styles from "./routeToCome.module.scss";
+import { useEffect } from "react";
 
 export const RouteToComePage = () => {
+  useEffect(() => {
+    if (window.kakao && window.kakao.maps) {
+      const container = document.getElementById("map");
+      const options = {
+        center: new window.kakao.maps.LatLng(36.1554, 128.431036),
+        level: 3,
+      };
+      const map = new window.kakao.maps.Map(container, options);
+
+      const markerPosition = new window.kakao.maps.LatLng(36.1554, 128.431036);
+
+      const marker = new window.kakao.maps.Marker({
+        position: markerPosition,
+      });
+
+      marker.setMap(map);
+    }
+  }, []);
+
   return (
     <div className={styles.container}>
-      <div className={styles.map}></div>
+      <div id="map" className={styles.map}></div>
       <div className={styles.title}>
         <h2>Location</h2>
         <p className={styles.location}>
@@ -14,21 +34,21 @@ export const RouteToComePage = () => {
         </p>
         <div className={styles.operation}>
           <h3>진료 시간</h3>
-          <p className={styles.operationInfo}>
+          <div className={styles.operationInfo}>
             평일
             <br />
             주말 및 공휴일
             <br /> 점심시간
-            <p className={styles.alert}>
+            <div className={styles.alert}>
               *입원환자 당일무료/외래환자 최대 2시간 지원가능
-            </p>
-          </p>
-          <p className={styles.park}>
+            </div>
+          </div>
+          <div className={styles.park}>
             <h3>주차장</h3>
             <p className={styles.parkInfo}>
               경상북도 구미시 산동읍 1426, 가람주차장
             </p>
-          </p>
+          </div>
         </div>
       </div>
       <ToolBar />
