d72741d4cbcfded4f6e23d469a765800cdee7d2b
minwoo1119 <minwoo1119@naver.com>
2025-09-15 11:29:52 +0900

    [Fix] #17 - 헤더 클릭 시 해당 페이지로 스크롤 기능 수정

--- src/components/common/headers/Header.tsx
+++ src/components/common/headers/Header.tsx
@@ -1,23 +1,59 @@
 import styles from './header.module.scss';
 
-export const Header = () => {
+type Props = {
+  sectionRefs: {
+    introduce: React.RefObject<HTMLDivElement>;
+    doctor: React.RefObject<HTMLDivElement>;
+    equipment: React.RefObject<HTMLDivElement>;
+    hospital: React.RefObject<HTMLDivElement>;
+    route: React.RefObject<HTMLDivElement>;
+  };
+};
+
+export const Header = ({ sectionRefs }: Props) => {
+  const handleScroll = (ref: React.RefObject<HTMLDivElement>) => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className={styles.container}>
       <img src='/header_logo.svg' alt='메인 로고' className={styles.logo} />
       <div className={styles.tabBox}>
-        <a href='#' className={styles.link}>
+        <a
+          href='#'
+          className={styles.link}
+          onClick={() => handleScroll(sectionRefs.introduce)}
+        >
           병원 소개
         </a>
-        <a href='#' className={styles.link}>
+        <a
+          href='#'
+          className={styles.link}
+          onClick={() => handleScroll(sectionRefs.doctor)}
+        >
           의료진 소개
         </a>
-        <a href='#' className={styles.link}>
+        <a
+          href='#'
+          className={styles.link}
+          onClick={() => handleScroll(sectionRefs.equipment)}
+        >
           장비 소개
         </a>
-        <a href='#' className={styles.link}>
+        <a
+          href='#'
+          className={styles.link}
+          onClick={() => handleScroll(sectionRefs.hospital)}
+        >
           병원 사진
         </a>
-        <a href='#' className={styles.link}>
+        <a
+          href='#'
+          className={styles.link}
+          onClick={() => handleScroll(sectionRefs.route)}
+        >
           오시는 길
         </a>
       </div>
