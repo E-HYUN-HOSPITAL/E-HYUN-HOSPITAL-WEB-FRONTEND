24a53b61eb3f5035229ecb339b45d724b9004912
minwoo1119 <minwoo1119@naver.com>
2025-09-15 11:33:32 +0900

    [Chore] #17 - 헤더 스크롤러 userRef 설정

--- src/page/mainPage/MainPage.tsx
+++ src/page/mainPage/MainPage.tsx
@@ -1,5 +1,6 @@
 import { DoctorInfoPage } from '../../components/doctor-info/page';
 import { EquipmentInfo } from '../../components/equipment-info/page';
+import { Footer } from '../../components/footer/Footer';
 import { Header } from '../../components/common/headers/Header';
 import { HospitalPicturePage } from '../../components/hospital-picture/page';
 import { IntroducePage } from '../../components/introduce/page';
@@ -7,18 +8,34 @@ import { MainPageComponent } from '../../components/main/page';
 import { PromisePage } from '../../components/promise/page';
 import { RouteToComePage } from '../../components/route-to-come/page';
 import styles from './mainpage.module.scss';
+import { useRef } from 'react';
 
 function MainPage() {
+  const introduceRef = useRef(null);
+  const doctorRef = useRef(null);
+  const equipmentRef = useRef(null);
+  const hospitalRef = useRef(null);
+  const routeRef = useRef(null);
+
+  const sectionRefs = {
+    introduce: introduceRef,
+    doctor: doctorRef,
+    equipment: equipmentRef,
+    hospital: hospitalRef,
+    route: routeRef,
+  };
+
   return (
     <div className={styles.container}>
-      <Header />
+      <Header sectionRefs={sectionRefs} />
       <MainPageComponent />
-      <IntroducePage />
+      <IntroducePage ref={introduceRef} />
       <PromisePage />
-      <DoctorInfoPage />
-      <EquipmentInfo />
-      <HospitalPicturePage />
+      <DoctorInfoPage ref={doctorRef} />
+      <EquipmentInfo ref={equipmentRef} />
+      <HospitalPicturePage ref={hospitalRef} />
       <RouteToComePage />
+      <Footer />
     </div>
   );
 }
