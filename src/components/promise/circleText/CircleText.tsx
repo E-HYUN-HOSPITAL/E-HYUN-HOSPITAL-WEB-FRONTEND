598f811a2c129b5cddad49b80148f0716b77214a
minwoo1119 <minwoo1119@naver.com>
2025-09-17 09:07:23 +0900

    [Refactor] #19 - CircleText description \n으로 줄바꿈

--- src/components/promise/circleText/CircleText.tsx
+++ src/components/promise/circleText/CircleText.tsx
@@ -7,11 +7,19 @@ type Props = {
 };
 
 export const CircleText = ({ number, title, description }: Props) => {
+  const descriptionLines = description.split('\n');
+
   return (
     <div className={styles.container}>
       <div className={styles.number}>{number}</div>
       <div className={styles.title}>{title}</div>
-      <div className={styles.description}>{description}</div>
+      <div className={styles.descriptionBox}>
+        {descriptionLines.map((line, index) => (
+          <div key={index} className={styles.description}>
+            {line}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
