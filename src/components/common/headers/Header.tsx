2fb65a05864ec7d0756d6041f041e1203fb3c643
minwoo1119 <minwoo1119@naver.com>
2025-09-17 09:40:23 +0900

    [Feat] #19 - 헤더 상단 고정

--- src/components/common/headers/Header.tsx
+++ src/components/common/headers/Header.tsx
@@ -8,9 +8,12 @@ type Props = {
     hospital: React.RefObject<HTMLDivElement>;
     route: React.RefObject<HTMLDivElement>;
   };
+
+  activeSection: string;
+  handleScroll: (ref: React.RefObject<HTMLDivElement | null>) => void;
 };
 
-export const Header = ({ sectionRefs }: Props) => {
+export const Header = ({ sectionRefs, activeSection }: Props) => {
   const handleScroll = (ref: React.RefObject<HTMLDivElement>) => {
     if (ref.current) {
       ref.current.scrollIntoView({ behavior: 'smooth' });
@@ -23,35 +26,45 @@ export const Header = ({ sectionRefs }: Props) => {
       <div className={styles.tabBox}>
         <a
           href='#'
-          className={styles.link}
+          className={`${styles.link} ${
+            activeSection === 'introduce' ? styles.active : ''
+          }`}
           onClick={() => handleScroll(sectionRefs.introduce)}
         >
           병원 소개
         </a>
         <a
           href='#'
-          className={styles.link}
+          className={`${styles.link} ${
+            activeSection === 'doctor' ? styles.active : ''
+          }`}
           onClick={() => handleScroll(sectionRefs.doctor)}
         >
           의료진 소개
         </a>
         <a
           href='#'
-          className={styles.link}
+          className={`${styles.link} ${
+            activeSection === 'equitment' ? styles.active : ''
+          }`}
           onClick={() => handleScroll(sectionRefs.equipment)}
         >
           장비 소개
         </a>
         <a
           href='#'
-          className={styles.link}
+          className={`${styles.link} ${
+            activeSection === 'hospital' ? styles.active : ''
+          }`}
           onClick={() => handleScroll(sectionRefs.hospital)}
         >
           병원 사진
         </a>
         <a
           href='#'
-          className={styles.link}
+          className={`${styles.link} ${
+            activeSection === 'route' ? styles.active : ''
+          }`}
           onClick={() => handleScroll(sectionRefs.route)}
         >
           오시는 길
