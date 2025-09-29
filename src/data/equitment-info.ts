317fb3e735d8fe761e2b55431e33cce6c748786a
seoyeonii <seoyeonii@ajou.ac.kr>
2025-09-29 12:46:18 +0900

    move fonts folder & add equipments webp files

--- src/data/equitment-info.ts
+++ src/data/equitment-info.ts
@@ -4,52 +4,52 @@ export interface EquipmentItem {
   image: string;
 }
 
-export type EquipmentCategory = 'hanbang' | 'yangbang' | 'skincare';
+export type EquipmentCategory = "hanbang" | "yangbang" | "skincare";
 
 export const EQUIPMENT_DATA: Record<EquipmentCategory, EquipmentItem[]> = {
   hanbang: [
     {
       number: 1,
-      name: '한방장비 1',
-      image: '/equipments/dummy_equipment.png',
+      name: "한방장비 1",
+      image: "/equipments/dummy_equipment.png",
     },
-    { number: 2, name: '한방장비 2', image: '/equipments/dummy_equipment.png' },
-    { number: 3, name: '한방장비 3', image: '/equipments/dummy_equipment.png' },
-    { number: 4, name: '한방장비 4', image: '/equipments/dummy_equipment.png' },
-    { number: 5, name: '한방장비 5', image: '/equipments/dummy_equipment.png' },
+    { number: 2, name: "한방장비 2", image: "/equipments/dummy_equipment.png" },
+    { number: 3, name: "한방장비 3", image: "/equipments/dummy_equipment.png" },
+    { number: 4, name: "한방장비 4", image: "/equipments/dummy_equipment.png" },
+    { number: 5, name: "한방장비 5", image: "/equipments/dummy_equipment.png" },
   ],
   yangbang: [
-    { number: 1, name: '양방장비 1', image: '/equipments/dummy_equipment.png' },
-    { number: 2, name: '양방장비 2', image: '/equipments/dummy_equipment.png' },
-    { number: 3, name: '양방장비 3', image: '/equipments/dummy_equipment.png' },
-    { number: 4, name: '양방장비 4', image: '/equipments/dummy_equipment.png' },
-    { number: 5, name: '양방장비 5', image: '/equipments/dummy_equipment.png' },
+    { number: 1, name: "양방장비 1", image: "/equipments/dummy_equipment.png" },
+    { number: 2, name: "양방장비 2", image: "/equipments/dummy_equipment.png" },
+    { number: 3, name: "양방장비 3", image: "/equipments/dummy_equipment.png" },
+    { number: 4, name: "양방장비 4", image: "/equipments/dummy_equipment.png" },
+    { number: 5, name: "양방장비 5", image: "/equipments/dummy_equipment.png" },
   ],
   skincare: [
     {
       number: 1,
-      name: '피부과장비 1',
-      image: '/equipments/dummy_equipment.png',
+      name: "Care5",
+      image: "/equipments/care5.webp",
     },
     {
       number: 2,
-      name: '피부과장비 2',
-      image: '/equipments/dummy_equipment.png',
+      name: "Clarity2",
+      image: "/equipments/clarity2.webp",
     },
     {
       number: 3,
-      name: '피부과장비 3',
-      image: '/equipments/dummy_equipment.png',
+      name: "LDM",
+      image: "/equipments/LDM.webp",
     },
     {
       number: 4,
-      name: '피부과장비 4',
-      image: '/equipments/dummy_equipment.png',
+      name: "picoPlus",
+      image: "/equipments/picoPlus.webp",
     },
     {
       number: 5,
-      name: '피부과장비 5',
-      image: '/equipments/dummy_equipment.png',
+      name: "Oligio",
+      image: "/equipments/올리지오.webp",
     },
   ],
 };
