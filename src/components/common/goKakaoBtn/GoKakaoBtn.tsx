b566750936957edd57abd27be430fc25ce9b1828
seoyeonii <seoyeonii@ajou.ac.kr>
2025-11-19 05:05:59 +0900

    [Refactor] #19 - GoKakaoBtn 오류 수정

--- src/components/common/goKakaoBtn/GoKakaoBtn.tsx
+++ src/components/common/goKakaoBtn/GoKakaoBtn.tsx
@@ -1,10 +1,16 @@
 import styles from "./goKakaoBtn.module.scss";
 
-export const GoKakaoBtn = () => {
+type Props = {
+  onClick?: () => void;
+};
+
+export const GoKakaoBtn = ({ onClick }: Props) => {
   return (
     <div
       className={styles.container}
-      onClick={() => window.open("https://pf.kakao.com/_hKrHn", "_blank")}
+      onClick={
+        onClick || (() => window.open("https://pf.kakao.com/_hKrHn", "_blank"))
+      }
       title="카카오톡 채널로 이동"
     >
       <img src="/kakao_icon.svg" alt="카카오 아이콘" />
