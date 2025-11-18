83e04ffa9c729da537003774de473b03d3b96e05
seoyeonii <seoyeonii@ajou.ac.kr>
2025-11-19 04:10:38 +0900

    [Feat] #1 - 메인 헤더 메뉴 변경 및 서브메뉴 등록

--- src/components/common/headers/Header.tsx
+++ src/components/common/headers/Header.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import styles from './header.module.scss';
+import React, { useState } from "react";
+import styles from "./header.module.scss";
+import { HEADER_MENU, type SubMenuItem } from "../../../data/menu";
 
 type Props = {
   sectionRefs: {
+    [key: string]: React.RefObject<HTMLDivElement | null>;
     introduce: React.RefObject<HTMLDivElement | null>;
     doctor: React.RefObject<HTMLDivElement | null>;
     equipment: React.RefObject<HTMLDivElement | null>;
@@ -14,70 +16,82 @@ type Props = {
 };
 
 export const Header = ({ sectionRefs, activeSection, handleScroll }: Props) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const goHome = () => {
+    window.location.href = "/";
+  };
+
+  const handleMenuClick = (e: React.MouseEvent, item: SubMenuItem) => {
+    if (item.type === "scroll") {
+      e.preventDefault();
+
+      const targetRef = sectionRefs[item.key];
+
+      if (targetRef) {
+        handleScroll(targetRef);
+      } else {
+        console.warn(`Ref not found for key: ${item.key}`);
+      }
+      setIsHovered(false);
+    } else {
+      setIsHovered(false);
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <img src='/header_logo.svg' alt='메인 로고' className={styles.logo} />
-      <div className={styles.tabBox}>
-        <a
-          href='#'
-          className={`${styles.link} ${
-            activeSection === 'introduce' ? styles.active : ''
-          }`}
-          onClick={(e) => {
-            e.preventDefault();
-            handleScroll(sectionRefs.introduce);
-          }}
-        >
-          병원 소개
-        </a>
-        <a
-          href='#'
-          className={`${styles.link} ${
-            activeSection === 'doctor' ? styles.active : ''
-          }`}
-          onClick={(e) => {
-            e.preventDefault();
-            handleScroll(sectionRefs.doctor);
-          }}
-        >
-          의료진 소개
-        </a>
-        <a
-          href='#'
-          className={`${styles.link} ${
-            activeSection === 'equipment' ? styles.active : ''
-          }`}
-          onClick={(e) => {
-            e.preventDefault();
-            handleScroll(sectionRefs.equipment);
-          }}
-        >
-          장비 소개
-        </a>
-        <a
-          href='#'
-          className={`${styles.link} ${
-            activeSection === 'hospital' ? styles.active : ''
-          }`}
-          onClick={(e) => {
-            e.preventDefault();
-            handleScroll(sectionRefs.hospital);
-          }}
-        >
-          병원 사진
-        </a>
-        <a
-          href='#'
-          className={`${styles.link} ${
-            activeSection === 'route' ? styles.active : ''
-          }`}
-          onClick={(e) => {
-            e.preventDefault();
-            handleScroll(sectionRefs.route);
-          }}
-        >
-          오시는 길
-        </a>
+    <div
+      className={`${styles.container} ${isHovered ? styles.open : ""}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className={styles.topBar}>
+        <img
+          src="/header_logo.svg"
+          alt="메인 로고"
+          className={styles.logo}
+          onClick={goHome}
+        />
+
+        <ul className={styles.mainNav}>
+          {HEADER_MENU.map((mainItem) => (
+            <li
+              key={mainItem.key}
+              className={styles.navItem}
+              onClick={() => {
+                if (mainItem.key === "intro_main") {
+                  goHome();
+                }
+              }}
+            >
+              <span>{mainItem.label}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className={styles.dropdownContainer}>
+        <div className={styles.dropdownContent}>
+          {HEADER_MENU.map((mainItem) => (
+            <ul key={mainItem.key} className={styles.subList}>
+              {mainItem.children.map((subItem) => (
+                <li key={subItem.label}>
+                  <a
+                    href={subItem.type === "link" ? subItem.key : "#"}
+                    className={`${styles.subLink} ${
+                      subItem.type === "scroll" && activeSection === subItem.key
+                        ? styles.active
+                        : ""
+                    }`}
+                    onClick={(e) => handleMenuClick(e, subItem)}
+                  >
+                    {subItem.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          ))}
+        </div>
       </div>
     </div>
   );
