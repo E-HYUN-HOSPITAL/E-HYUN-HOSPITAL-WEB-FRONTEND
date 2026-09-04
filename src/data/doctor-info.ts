9b03b7bb0785d1760c427088f64733995041a14d
seoyeonii <seoyeonii@ajou.ac.kr>
2026-09-04 16:25:36 +0900

    [Feat] #3 - 의료진 업데이트

--- src/data/doctor-info.ts
+++ src/data/doctor-info.ts
@@ -47,27 +47,6 @@ export const DOCTOR_DATA: { [key: string]: DoctorType } = {
       "전 이안한방병원 진료원장",
     ],
   },
-  유동곤: {
-    major: "Clinic Director",
-    imageUrl: "/doctor-info/clinic_director.webp",
-    position: "진료원장",
-    specialty: "한의사",
-    education: [
-      "미국진단초음파협회(ARDMS)",
-      "국제근골격계초음파 진단 자격자(RMSK)",
-      "대한스포츠한의학회 팀닥터 과정 수료",
-    ],
-    memberships: [
-      "대한한의학회 회원",
-      "대한스포츠한의학회 회원",
-      "한방비만학회 회원",
-      "전 경희더한의원 진료원장",
-      "전 동편부부한의원 진료원장",
-      "전 청라한방병원 진료원장",
-      "전 제일한의원 진료원장",
-      "전 제일한방병원 진료원장",
-    ],
-  },
   황미나: {
     major: "Clinic Director",
     imageUrl: null,
@@ -114,11 +93,11 @@ export const DOCTOR_DATA: { [key: string]: DoctorType } = {
       "Recent Domestic and International Trends in Clinical Research of Herbal Medicine Treatment for Crohn’s disease",
     ],
   },
-  마영진: {
+  김려원: {
     major: "Clinic Director",
     imageUrl: null,
-    position: "비뇨기과 전문의",
-    specialty: "의사",
+    position: "가정의학과 원장",
+    specialty: "가정의학과 원장",
     education: null,
     memberships: null,
   },
